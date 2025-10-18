ea708fb00cdd3aa7dddf6586bf43b8bd56545bd7
ek.belikova <ek.belikova@tbank.ru>
2025-10-18 17:08:50 +0300

    add new fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
 package org.example
 
+import org.example.syntax.Person
+
 fun main() {
     println("Hello World!")
+    val person = Person("Kate", 22)
+    person.introduce()
+    person.checkAgeCategory()
 }
